9e7a17609173cd4818695e6fcb905fbbe17b52fb
Nikhil Maurya <157352749+Nikhil-Maurya01@users.noreply.github.com>
2025-10-24 20:34:24 +0530

    Refactor EvenOrOdd.java for clarity and resource management
    Added comments for clarity and closed the scanner.

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -2,15 +2,20 @@ import java.util.Scanner;
 
 public class EvenOrOdd {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter a number :");
-        int num=sc.nextInt();
-        if(num%2==0){
+        Scanner sc = new Scanner(System.in);
+
+        // Prompt user to enter a number
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+
+        // Check if the number is even or odd using modulus operator
+        if (num % 2 == 0) {
             System.out.println("Number is even");
-        }
-        else{
+        } else {
             System.out.println("Number is odd");
         }
-    }
 
+        sc.close(); // Close the scanner to avoid resource leak
+    }
 }
+``
