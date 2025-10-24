1697da4a0ce24d38c09799c81936fee59a9fc024
Nikhil Maurya <157352749+Nikhil-Maurya01@users.noreply.github.com>
2025-10-24 20:39:27 +0530

    Refactor multiplication table display format
    Updated the output format for better readability and added a header.

--- MultipleTables.java
+++ MultipleTables.java
@@ -1,11 +1,25 @@
 public class MultipleTables {
     public static void main(String[] args) {
-        System.out.println("These are numeric tables from 1 to 10 :");
+        // Display header
+        System.out.println("These are numeric tables from 1 to 10:");
         System.out.println();
         System.out.println("--------------------------------------------------------------------------");
         System.out.println();
-        for(int i=1;i<=10;i++){
-            System.out.println((1*i)+"\t"+(2*i)+"\t"+(3*i)+"\t"+(4*i)+"\t"+(5*i)+"\t"+(6*i)+"\t"+(7*i)+"\t"+(8*i)+"\t"+(9*i)+"\t"+(10*i));
+
+        // Print multiplication tables from 1 to 10 side by side
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(
+                (1 * i) + "\t" +
+                (2 * i) + "\t" +
+                (3 * i) + "\t" +
+                (4 * i) + "\t" +
+                (5 * i) + "\t" +
+                (6 * i) + "\t" +
+                (7 * i) + "\t" +
+                (8 * i) + "\t" +
+                (9 * i) + "\t" +
+                (10 * i)
+            );
         }
     }
 }
