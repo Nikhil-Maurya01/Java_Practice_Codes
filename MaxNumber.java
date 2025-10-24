4fafd54063e2333dcc5dc768405f2ee21fb9fd2e
Nikhil Maurya <157352749+Nikhil-Maurya01@users.noreply.github.com>
2025-10-24 20:38:11 +0530

    Refactor MaxNumber.java for readability and comments
    Refactor code for clarity and add comments.

--- MaxNumber.java
+++ MaxNumber.java
@@ -1,27 +1,37 @@
 import java.util.Scanner;
+
 public class MaxNumber {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter 5 numbers :");
-        int n1=sc.nextInt();
-        int n2=sc.nextInt();
-        int n3=sc.nextInt();
-        int n4=sc.nextInt();
-        int n5=sc.nextInt();
-        int max=n1;
-        if(max<n2){
-            max=n2;
+        Scanner sc = new Scanner(System.in);
+
+        // Prompt user to enter 5 numbers
+        System.out.println("Enter 5 numbers:");
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+        int n4 = sc.nextInt();
+        int n5 = sc.nextInt();
+
+        // Assume first number is the maximum
+        int max = n1;
+
+        // Compare with other numbers to find the maximum
+        if (max < n2) {
+            max = n2;
         }
-        if(max<n3){
-            max=n3;
+        if (max < n3) {
+            max = n3;
         }
-        if(max<n4){
-            max=n4;
+        if (max < n4) {
+            max = n4;
         }
-        if(max<n5){
-            max=n5;
+        if (max < n5) {
+            max = n5;
         }
-        System.out.println("Biggest number is :"+max);
+
+        // Display the largest number
+        System.out.println("Biggest number is: " + max);
+
+        sc.close(); // Close the scanner to avoid resource leak
     }
-    
 }
