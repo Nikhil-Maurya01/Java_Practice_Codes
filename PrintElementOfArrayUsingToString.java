d825ae2b91d1ce1d5a90a8cd86acd7fc6556b29d
Nikhil Maurya <157352749+Nikhil-Maurya01@users.noreply.github.com>
2025-10-24 20:50:38 +0530

    Update PrintElementOfArrayUsingToString.java

--- PrintElementOfArrayUsingToString.java
+++ PrintElementOfArrayUsingToString.java
@@ -1,15 +1,22 @@
-import java.util.Arrays;
-import java.util.Scanner;
 public class PrintElementOfArrayUsingToString {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-		System.out.println("Enter length of array:");
-		int n=sc.nextInt();
-        int arr[]=new int[n];
+        Scanner sc = new Scanner(System.in);
+
+        // Prompt user to enter the length of the array
+        System.out.println("Enter length of array:");
+        int n = sc.nextInt(); // Read the array size
+
+        int arr[] = new int[n]; // Declare an integer array of size 'n'
+
+        // Prompt user to enter elements of the array
         System.out.println("Enter elements of array:");
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt(); // Read each element and store in array
         }
+
+        // Print the array using Arrays.toString() method
         System.out.println(Arrays.toString(arr));
+
+        sc.close(); // Close the scanner to avoid resource leak
     }
 }
