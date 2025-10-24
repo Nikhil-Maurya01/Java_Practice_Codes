3f026c1ba37b175eb50249a4fc317e2e6256e1c0
Nikhil Maurya <157352749+Nikhil-Maurya01@users.noreply.github.com>
2025-10-24 20:26:21 +0530

    Improve readability and add comments in ArmstrongNumber
    Refactor Armstrong number program for clarity and comments.

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -2,28 +2,40 @@ import java.util.Scanner;
 
 public class ArmstrongNumber {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter any number :");
-        int num=sc.nextInt();
-        int savenum=num;
-        int savenum2=num;
-        int rem=0;
-        double sum=0;
-        int dig=0;
-        while(num>0){
-            num=num/10;
+        Scanner sc = new Scanner(System.in);
+
+        // Prompt user to enter a number
+        System.out.println("Enter any number:");
+        int num = sc.nextInt();
+
+        // Save original number for final comparison
+        int savenum = num;
+        int savenum2 = num;
+
+        int rem = 0;       // To store remainder (digit)
+        double sum = 0;    // To store sum of digits raised to power of total digits
+        int dig = 0;       // To count number of digits
+
+        // Count number of digits in the number
+        while (num > 0) {
+            num = num / 10;
             dig++;
         }
-        while(savenum>0){
-            rem=savenum%10;
-            sum=sum+(Math.pow(rem,dig));
-            savenum=savenum/10;
+
+        // Calculate sum of digits raised to the power of total digits
+        while (savenum > 0) {
+            rem = savenum % 10;
+            sum = sum + Math.pow(rem, dig);
+            savenum = savenum / 10;
         }
-        if(savenum2==sum){
-            System.out.println("It is an armstrong number");
+
+        // Check if the original number is equal to the calculated sum
+        if (savenum2 == sum) {
+            System.out.println("It is an Armstrong number");
+        } else {
+            System.out.println("Not an Armstrong number");
         }
-        else{
-            System.out.println("Not an armstrong number");
-        }
+
+        sc.close(); // Close the scanner to prevent resource leak
     }
 }
