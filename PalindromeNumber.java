c077598221fd98f2aae7d568427bd0c769772e91
Nikhil Maurya <157352749+Nikhil-Maurya01@users.noreply.github.com>
2025-10-24 20:42:14 +0530

    Improve readability of PalindromeNumber.java
    Refactor PalindromeNumber.java for better readability and structure.

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -2,22 +2,30 @@ import java.util.Scanner;
 
 public class PalindromeNumber {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter any number :");
-        int num=sc.nextInt();
-        int savenum=num;
-        int rem=0;
-        int rev=0;
-        while(num>0){
-            rem=num%10;
-            rev=rev*10+rem;
-            num=num/10;
+        Scanner sc = new Scanner(System.in);
+
+        // Prompt user to enter a number
+        System.out.println("Enter any number:");
+        int num = sc.nextInt();
+
+        int savenum = num; // Store original number for final comparison
+        int rem = 0;        // To store remainder (digit)
+        int rev = 0;        // To store reversed number
+
+        // Reverse the number
+        while (num > 0) {
+            rem = num % 10;         // Get last digit
+            rev = rev * 10 + rem;   // Append digit to reversed number
+            num = num / 10;         // Remove last digit
         }
-        if(savenum==rev){
+
+        // Check if original and reversed numbers are equal
+        if (savenum == rev) {
             System.out.println("Palindrome");
-        }
-        else{
+        } else {
             System.out.println("Not a palindrome");
         }
+
+        sc.close(); // Close the scanner to avoid resource leak
     }
 }
