b268178753cf7a76413879f688a4151cd4cdbd2f
Nikhil Maurya <157352749+Nikhil-Maurya01@users.noreply.github.com>
2025-10-24 20:40:27 +0530

    Refactor digit multiplication logic in Java program
    Refactor code to improve readability and variable naming.

--- MultiplicationOfDigitsOfNumber.java
+++ MultiplicationOfDigitsOfNumber.java
@@ -2,17 +2,26 @@ import java.util.Scanner;
 
 public class MultiplicationOfDigitsOfNumber {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter any number :");
-        int num=sc.nextInt();
-        int a=num;
-        int rem=0;
-        int sum=1;
-        while(num>0){
-            rem=num%10;
-            sum=sum*rem;
-            num=num/10;
+        Scanner sc = new Scanner(System.in);
+
+        // Prompt user to enter a number
+        System.out.println("Enter any number:");
+        int num = sc.nextInt();
+
+        int a = num;       // Store original number for display
+        int rem = 0;       // To store each digit
+        int product = 1;   // To store the product of digits
+
+        // Loop to extract digits and multiply them
+        while (num > 0) {
+            rem = num % 10;        // Get last digit
+            product = product * rem; // Multiply with current product
+            num = num / 10;        // Remove last digit
         }
-        System.out.println("Sum of digits of "+a+" is = "+sum);
+
+        // Display result
+        System.out.println("Product of digits of " + a + " is = " + product);
+
+        sc.close(); // Close the scanner to avoid resource leak
     }
 }
