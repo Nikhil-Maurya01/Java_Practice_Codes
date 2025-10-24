5084c67b9574575b47a664542bdc5edf7f38895e
Nikhil Maurya <157352749+Nikhil-Maurya01@users.noreply.github.com>
2025-10-24 20:41:35 +0530

    Improve NumberOfDigits.java readability and comments
    Refactor code for clarity and add comments.

--- NumberOfDigits.java
+++ NumberOfDigits.java
@@ -2,15 +2,24 @@ import java.util.Scanner;
 
 public class NumberOfDigits {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter any number :");
-        int num=sc.nextInt();
-        int a=num;
-        int i=0;
-        while(num>0){
-            num=num/10;
+        Scanner sc = new Scanner(System.in);
+
+        // Prompt user to enter a number
+        System.out.println("Enter any number:");
+        int num = sc.nextInt();
+
+        int a = num; // Store original number for display
+        int i = 0;   // Counter for number of digits
+
+        // Loop to count digits by dividing the number by 10 repeatedly
+        while (num > 0) {
+            num = num / 10;
             ++i;
         }
-        System.out.println("Number of digits in "+a+" is "+i);
+
+        // Display the result
+        System.out.println("Number of digits in " + a + " is " + i);
+
+        sc.close(); // Close the scanner to avoid resource leak
     }
 }
