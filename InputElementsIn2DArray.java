17e50d882ee0f1bc43fbfb6ccdb69670a84d7994
Nikhil Maurya <157352749+Nikhil-Maurya01@users.noreply.github.com>
2025-10-24 20:36:26 +0530

    Refactor InputElementsIn2DArray with comments and scanner close
    Added comments for better code clarity and closed the scanner.

--- InputElementsIn2DArray.java
+++ InputElementsIn2DArray.java
@@ -1,25 +1,37 @@
 import java.util.Scanner;
+
 public class InputElementsIn2DArray {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-		System.out.println("Enter rows of array:");
-		int r=sc.nextInt();
-        System.out.println("Enter columns of array");
-        int c=sc.nextInt();
-        int arr[][]=new int[r][c];
-        System.out.println("Input elements of array");
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                arr[i][j]=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+
+        // Prompt user to enter number of rows
+        System.out.println("Enter rows of array:");
+        int r = sc.nextInt();
+
+        // Prompt user to enter number of columns
+        System.out.println("Enter columns of array:");
+        int c = sc.nextInt();
+
+        // Declare and initialize 2D array
+        int arr[][] = new int[r][c];
+
+        // Input elements into the 2D array
+        System.out.println("Input elements of array:");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                arr[i][j] = sc.nextInt();
             }
         }
-        System.out.println("Array is :");
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-            System.out.print(arr[i][j]+"\t");
+
+        // Display the 2D array
+        System.out.println("Array is:");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                System.out.print(arr[i][j] + "\t");
             }
-            System.out.println();
+            System.out.println(); // Move to next row
         }
+
+        sc.close(); // Close the scanner to avoid resource leak
     }
 }
-
